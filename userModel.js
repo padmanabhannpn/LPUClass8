55c5ffe5680c4457b178fa441e652bdeb6640853
Padmanabhan N <padmanabhan@Padmanabhans-MacBook-Pro.local>
2025-06-26 11:50:31 +0530

    Adding Validations

--- userModel.js
+++ userModel.js
@@ -2,7 +2,13 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
 
-    name: String,
+    name: {
+        type:String,
+        required:[true,"Name is required"],
+        minlength:[3,"Name must be in atleast 3 Char"],
+        maxlength:[50,"Name must be less than 50 Char"]
+    }
+    ,
     subject : String
 
 });
